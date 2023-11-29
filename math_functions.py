b2c4c8431d37040cc4aceb044461f44a21b36a02
Andrew <toulchinski@gmail.com>
2023-11-29 11:34:16 +0200

    code formated

--- math_functions.py
+++ math_functions.py
@@ -1,8 +1,10 @@
 def add(x, y):
     return x + y
 
+
 def multiply(x, y):
     return x * y
+
 
 def power(x, y):
     """Calculate x raised to the power y without using math.pow."""
@@ -10,4 +12,3 @@ def power(x, y):
     for _ in range(int(y)):
         result *= x
     return result
-
