a82756e4c51fc23dd14e6bc5ef025460f6b6d962
Andrew <toulchinski@gmail.com>
2023-11-29 12:02:08 +0200

    Chenged assert function for pytest compatible

--- test_math_functions.py
+++ test_math_functions.py
@@ -5,10 +5,13 @@ from math_functions import add, multiply, power
 
 class TestMathFunctions(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(5, 4), 9)
-        self.assertEqual(add(5, 5), 10)
-        self.assertEqual(3, add(2, 1))
-        self.assertEqual(-1, add(2, -3))
+        assert add(1, 2) == 3
+        assert add(2, -3) == -1
+
+        # self.assertEqual(add(5, 4), 9)
+        # self.assertEqual(add(5, 5), 10)
+        # self.assertEqual(3, add(2, 1))
+        # self.assertEqual(-1, add(2, -3))
 
     def test_multiply(self):
         self.assertEqual(multiply(3, 4), 12)
